92e3b2b82368f13abbd3a4cee5c5dacb117672b6
TSD066 <tsd066@tsd066.tribalscale.lan>
2018-07-17 11:30:48 -0400

    Added promise library

--- server.js
+++ server.js
@@ -3,35 +3,49 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const app = express();
 const sharp = require('sharp');
- 
+const Promise = require("bluebird");
+
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
 // default options
 app.use(fileUpload());
 
-const splitImage = function(filename, extention, timestamp) {
-    sharp(`./uploadedImages/${filename}_${timestamp}.${extention}`)
-        .extract({ left: 0, top: 0, width: 666, height: 666 })
-        .toFile(`./uploadedImages/${filename}_${timestamp}_1.${extention}`, function(err) {
-            // Extract a region of the input image, saving in the same format.
-            console.log('err', err);
-            return { success: "works" };
-        });
+const splitImage = function (filename, extention, timestamp) {
+  return new Promise(function (resolve, reject) {
+    const image = sharp(`./uploadedImages/${filename}_${timestamp}.${extention}`);
+    image
+      .metadata()
+      .then(function (metadata) {
+        console.log(metadata);
+        let numImage = metadata.width / metadata.height;
+        for(let i = 0; i < )
+        return image
+          .extract({ left: 0, top: 0, width: 666, height: 666 })
+          .toFile(`./uploadedImages/${filename}_${timestamp}_1.${extention}`);
+      })
+      .then(function (info, err) {
+        console.log(err);
+        if (err) {
+          reject({ status: "failed" });
+        }
+        resolve({ status: "success" });
+      });
+  });
 };
 
-app.post('/upload', function(req, res) {
+app.post('/upload', function (req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
- 
+
   let file = req.files.imageFile;
 
-  const filename = file.name.split('.')[0];  
+  const filename = file.name.split('.')[0];
   const extention = file.name.split('.')[1];
   const timestamp = (new Date()).valueOf();
 
   // Use the mv() method to place the file somewhere on your server
-  file.mv(`./uploadedImages/${filename}_${timestamp}.${extention}`, function(err) {
+  file.mv(`./uploadedImages/${filename}_${timestamp}.${extention}`, function (err) {
     if (err)
       return res.status(500).send(err);
 
@@ -42,5 +56,5 @@ app.post('/upload', function(req, res) {
 });
 
 app.listen(3000, () => {
-    console.log(`Server is listening on port 3000`);
+  console.log(`Server is listening on port 3000`);
 });
