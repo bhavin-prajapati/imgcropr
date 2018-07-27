b286882579b7d8f4288dcd5516524bcb435f33fe
TSD066 <tsd066@tsd066.tribalscale.lan>
2018-07-27 10:49:54 -0400

    Added an image viewer with zooming capability

--- client/src/pages/crop.js
+++ client/src/pages/crop.js
@@ -7,26 +7,11 @@ import styled from 'react-emotion';
 const OutsideWrapper = styled.div`
     height: auto;
     margin:20px 60px;
-    border:1px solid blue;
 `;
 
-const InsideWrapper = styled.div`
-    position:relative;
-`;
-
-const CoveredImage = styled.img`
+const ImageCanvas = styled.canvas`
     position: relative;
-    top:0px;
-    left:0px;
-    margin: 0;
-`;
-
-const CoveringCanvas = styled.canvas`
-    position: absolute;
-    top:0px;
-    left:0px;
-    width: 100%;
-    height: 100%;
+    border: 1px solid blue;
 `;
 
 export class CropPage extends Component {
@@ -37,13 +22,14 @@ export class CropPage extends Component {
     this.mouseUp = this.mouseUp.bind(this);
     this.mouseMove = this.mouseMove.bind(this);
     this.draw = this.draw.bind(this);
+    this.trackTransforms = this.trackTransforms.bind(this);
   }
 
   componentWillMount() {
     let params = queryString.parse(location.search);
 
     let img = new Image;
-    img.onload = function(){
+    img.onload = function () {
       this.setState({
         imageHeight: img.height,
         imageWidth: img.width,
@@ -57,25 +43,148 @@ export class CropPage extends Component {
       rects: [],
       selectRect: {},
       drag: false,
+      imageSource: `http://localhost:3000/${params.image}`,
       imageHeight: 0,
       imageWidth: 0,
     });
   }
 
   componentDidMount() {
-    const canvas = document.getElementById('canvas'),
-      ctx = canvas.getContext('2d');
+    const canvas = document.getElementById('imageCanvas');
+    canvas.width = 1280;
+    canvas.height = 800;
 
-    canvas.addEventListener('mousedown', this.mouseDown, false);
-    canvas.addEventListener('mouseup', this.mouseUp, false);
-    canvas.addEventListener('mousemove', this.mouseMove, false);
+    let img = new Image;
 
-    this.setState({
-      canvas,
-      ctx
-    });
+    img.onload = function () {
 
-    this.draw(ctx);
+      let ctx = canvas.getContext('2d');
+      this.trackTransforms(ctx);
+
+      function redraw() {
+        // Clear the entire canvas
+        let p1 = ctx.transformedPoint(0, 0);
+        let p2 = ctx.transformedPoint(canvas.width, canvas.height);
+        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.restore();
+        ctx.drawImage(img, 0, 0);
+      }
+      redraw();
+
+      let lastX = canvas.width / 2, lastY = canvas.height / 2;
+      let dragStart, dragged;
+
+      canvas.addEventListener('mousedown', function (evt) {
+        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        dragStart = ctx.transformedPoint(lastX, lastY);
+        dragged = false;
+      }, false);
+
+      canvas.addEventListener('mousemove', function (evt) {
+        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        dragged = true;
+        if (dragStart) {
+          var pt = ctx.transformedPoint(lastX, lastY);
+          ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
+          redraw();
+        }
+      }, false);
+
+      canvas.addEventListener('mouseup', function (evt) {
+        dragStart = null;
+        if (!dragged) zoom(evt.shiftKey ? -1 : 1);
+      }, false);
+
+      let scaleFactor = 1.1;
+
+      let zoom = function (clicks) {
+        var pt = ctx.transformedPoint(lastX, lastY);
+        ctx.translate(pt.x, pt.y);
+        var factor = Math.pow(scaleFactor, clicks);
+        ctx.scale(factor, factor);
+        ctx.translate(-pt.x, -pt.y);
+        redraw();
+      }
+
+      let handleScroll = function (evt) {
+        var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+        if (delta) zoom(delta);
+        return evt.preventDefault() && false;
+      };
+
+      canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+      canvas.addEventListener('mousewheel', handleScroll, false);
+    }.bind(this);
+
+    img.src = this.state.imageSource;
+  }
+
+  trackTransforms(ctx) {
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    var xform = svg.createSVGMatrix();
+    ctx.getTransform = function () { return xform; };
+
+    var savedTransforms = [];
+    var save = ctx.save;
+    ctx.save = function () {
+      savedTransforms.push(xform.translate(0, 0));
+      return save.call(ctx);
+    };
+
+    var restore = ctx.restore;
+    ctx.restore = function () {
+      xform = savedTransforms.pop();
+      return restore.call(ctx);
+    };
+
+    var scale = ctx.scale;
+    ctx.scale = function (sx, sy) {
+      xform = xform.scaleNonUniform(sx, sy);
+      return scale.call(ctx, sx, sy);
+    };
+
+    var rotate = ctx.rotate;
+    ctx.rotate = function (radians) {
+      xform = xform.rotate(radians * 180 / Math.PI);
+      return rotate.call(ctx, radians);
+    };
+
+    var translate = ctx.translate;
+    ctx.translate = function (dx, dy) {
+      xform = xform.translate(dx, dy);
+      return translate.call(ctx, dx, dy);
+    };
+
+    var transform = ctx.transform;
+    ctx.transform = function (a, b, c, d, e, f) {
+      var m2 = svg.createSVGMatrix();
+      m2.a = a; m2.b = b; m2.c = c; m2.d = d; m2.e = e; m2.f = f;
+      xform = xform.multiply(m2);
+      return transform.call(ctx, a, b, c, d, e, f);
+    };
+
+    var setTransform = ctx.setTransform;
+    ctx.setTransform = function (a, b, c, d, e, f) {
+      xform.a = a;
+      xform.b = b;
+      xform.c = c;
+      xform.d = d;
+      xform.e = e;
+      xform.f = f;
+      return setTransform.call(ctx, a, b, c, d, e, f);
+    };
+
+    var pt = svg.createSVGPoint();
+    ctx.transformedPoint = function (x, y) {
+      pt.x = x; pt.y = y;
+      return pt.matrixTransform(xform.inverse());
+    }
   }
 
   mouseDown(e) {
@@ -92,7 +201,6 @@ export class CropPage extends Component {
       selectRect: currentRect,
       drag: true
     });
-
     this.draw(ctx);
   }
 
@@ -123,11 +231,11 @@ export class CropPage extends Component {
 
     if (drag) {
       const canvas = document.getElementById('canvas');
-      
+
       let currentRect = selectRect;
       currentRect.w = (e.pageX - canvas.offsetLeft) - selectRect.startX;
-      currentRect.h = (e.pageY - canvas.offsetTop) - selectRect.startY ;
-      
+      currentRect.h = (e.pageY - canvas.offsetTop) - selectRect.startY;
+
       this.setState({
         selectRect: currentRect
       });
@@ -143,26 +251,18 @@ export class CropPage extends Component {
       imageUrl
     } = this.state
 
-    // Draw image
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let img = new Image;
-    img.onload = function(){
-      ctx.drawImage(img, 0, 0);
-    };
-    img.src = imageUrl;
-
     // Draw all selections
     for (let i = 0; i < rects.length; i++) {
       let r = rects[i];
-      ctx.fillRect(r.startX, r.startY, r.w, r.h);
+      ctx.rect(r.startX, r.startY, r.w, r.h);
     }
-    
+
     // Draw current selection
-    if(Object.keys(selectRect).length > 0) {
-      ctx.fillRect(selectRect.startX, selectRect.startY, selectRect.w, selectRect.h);
+    if (Object.keys(selectRect).length > 0) {
+      ctx.rect(selectRect.startX, selectRect.startY, selectRect.w, selectRect.h);
     }
   }
-  
+
   cropImage(image) {
     axios.get(`http://localhost:3000/crop?image=${image}`)
       .then(function (response) {
@@ -175,20 +275,20 @@ export class CropPage extends Component {
 
   render() {
     let page = "";
-    if(Object.keys(this.state.params).length === 0) {
+    if (Object.keys(this.state.params).length === 0) {
       page = (
         <div>
           <h1>Hi folks!</h1>
           <p>Welcome to imgcropr.</p>
           <p>Upload an image to begin.</p>
-    
+
           <form
             id='uploadForm'
-            action='http://localhost:3000/upload' 
+            action='http://localhost:3000/upload'
             method='post'
             encType="multipart/form-data">
-              <input type="file" name="imageFile" />
-              <input type='submit' value='Upload!' />
+            <input type="file" name="imageFile" />
+            <input type='submit' value='Upload!' />
           </form>
         </div>
       );
@@ -197,16 +297,13 @@ export class CropPage extends Component {
         <div>
           <p>Click the hamburger to perform to modify this image.</p>
           <OutsideWrapper>
-              <InsideWrapper>
-                <CoveredImage src={`http://localhost:3000/${this.state.params.image}`} />
-                <CoveringCanvas id="canvas" />
-              </InsideWrapper>
+            <ImageCanvas id="imageCanvas" />
           </OutsideWrapper>
           <input type='button' value='Crop' onClick={() => { this.cropImage(params.image); }} />
         </div>
       );
     }
-    
+
     return page;
   };
 }
